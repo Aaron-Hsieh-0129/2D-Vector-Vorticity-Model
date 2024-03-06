a4de1e7767fafcfdbe6be68eef841f2b863f9f9b
Aaron@39 <b08209006@ntu.edu.tw>
2024-03-06 11:42:33 +0800

    Add switch to water forecast

--- src/Outputfile.cpp
+++ src/Outputfile.cpp
@@ -124,9 +124,6 @@ void Output::output_nc(int n, vvmArray &model) {
 	NcVar qrData = dataFile.addVar("qr", ncDouble, xzDim);
 	NcVar qvData = dataFile.addVar("qv", ncDouble, xzDim);
 	NcVar qrAccData = dataFile.addVar("qrAcc", ncDouble, xDim);
-	NcVar accretionData = dataFile.addVar("accretion", ncDouble, xzDim);
-	NcVar autoconversionData = dataFile.addVar("autoconversion", ncDouble, xzDim);
-	NcVar evaporationData = dataFile.addVar("evaporation", ncDouble, xzDim);
 
 	thData.putVar(model.th);
 	zetaData.putVar(model.zeta);
@@ -136,9 +133,15 @@ void Output::output_nc(int n, vvmArray &model) {
 	qrData.putVar(model.qr);
 	qvData.putVar(model.qv);
 	qrAccData.putVar(model.qrAcc);
-	accretionData.putVar(model.accretion);
-	autoconversionData.putVar(model.autoconversion);
-	evaporationData.putVar(model.evaporation);
+
+	#if defined(WATER)
+		NcVar accretionData = dataFile.addVar("accretion", ncDouble, xzDim);
+		NcVar autoconversionData = dataFile.addVar("autoconversion", ncDouble, xzDim);
+		NcVar evaporationData = dataFile.addVar("evaporation", ncDouble, xzDim);
+		accretionData.putVar(model.accretion);
+		autoconversionData.putVar(model.autoconversion);
+		evaporationData.putVar(model.evaporation);
+	#endif
 
 	if (n == 0) {
 		NcVar tbData = dataFile.addVar("tb", ncDouble, zNcDim);
